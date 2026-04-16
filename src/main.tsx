cee75e44b8a3f7e5d4c33a0301b4720d59ac547a
MichaelPopov62 <romantikzizni@gmail.com>
2026-04-16 13:14:36 +0300

    Змінив іконку

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "swiper/css";
 import "swiper/css/pagination";
+import "modern-normalize/modern-normalize.css";
 import "./global.css";
 
 const el = document.getElementById("root");
@@ -10,7 +11,7 @@ if (!el) {
   throw new Error("Элемент #root не найден");
 }
 
-createRoot(el).render(
+createRoot(el as HTMLDivElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
